8387cb9729481fe85d6fcdff8182ebc5457e249c
Your Name <karkibishal00@gmail.com>
2022-08-04 13:58:23 +1000

    created login, register, header, footer and implied react-router-dom in main  file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
-import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { LoginPage } from "./pages/login/Login.js";
+import { adminRegistration } from "./pages/adminRegistration/adminRegistration.js";
 
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/register" element={<adminRegistration />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/adminRegistration/adminRegistration.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const adminRegistration = () => {
+  return <div>adminRegistration</div>;
+};
