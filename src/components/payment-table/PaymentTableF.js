45d67967f66ceafd642631f83082baf23eeac857
Your Name <karkibishal00@gmail.com>
2022-08-31 11:12:14 +1000

    edit payment method success

--- src/components/payment-table/PaymentTableF.js
+++ src/components/payment-table/PaymentTableF.js
@@ -1,10 +1,12 @@
 import React, { useEffect } from "react";
 import { Button, Table } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
+import { EditPaymentM } from "../../pages/payment-method-form/EditPM";
 import {
   deletePaymentMethod,
   getPaymentMethod,
 } from "../../pages/payment-method/PaymentAction";
+import { setModalShow } from "../../pages/system-st/SystemSlice";
 
 export const PaymentTableF = () => {
   const dispatch = useDispatch();
@@ -22,8 +24,13 @@ export const PaymentTableF = () => {
     }
   };
 
+  const handleOnEdit = (_id) => {
+    dispatch(setModalShow(_id));
+  };
+
   return (
     <div>
+      <EditPaymentM />
       <Table striped bordered hover className="mt-4">
         <thead>
           <tr>
@@ -37,19 +44,17 @@ export const PaymentTableF = () => {
             <tr key={item._id}>
               <td> {i + 1} </td> <td> {item.status} </td> <td> {item.name} </td>{" "}
               <td>
-                <Button variant="info">
+                <Button variant="info" onClick={() => handleOnEdit(item._id)}>
                   {" "}
                   <i className="fa-solid fa-edit"> </i> Edit{" "}
                 </Button>{" "}
-                <Button variant="danger" type="button">
+                <Button
+                  variant="danger"
+                  type="submit"
+                  onClick={() => handleOnDelete(item._id)}
+                >
                   {" "}
-                  <i
-                    className="fa-solid fa-trash"
-                    onClick={() => handleOnDelete(item._id)}
-                  >
-                    {" "}
-                  </i>{" "}
-                  Delete{" "}
+                  <i className="fa-solid fa-trash"> </i> Delete{" "}
                 </Button>{" "}
               </td>{" "}
             </tr>
---
+++ src/pages/payment-method-form/EditPM.js
@@ -0,0 +1,79 @@
+import React, { useState } from "react";
+import { Button, Form } from "react-bootstrap";
+import { useDispatch } from "react-redux";
+import { CustomInoutFiedld } from "../../components/customInoutField/CustomInoutFiedld";
+import { CustomModal } from "../../components/modal/modal";
+import { postPaymentMethod } from "../payment-method/PaymentAction";
+
+const initialState = {
+  name: "",
+  status: "",
+  description: "",
+};
+export const EditPaymentM = () => {
+  const dispatch = useDispatch();
+  const [form, setForm] = useState(initialState); //form is an object
+
+  const handleOnChange = (e) => {
+    const { name, value, checked } = e.target;
+
+    if (name === "status") {
+      value = checked ? "Active" : "Inactive";
+    }
+
+    setForm({
+      ...form,
+      [name]: value,
+    });
+  };
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    // console.log(form);
+    dispatch(postPaymentMethod(form));
+  };
+
+  const inputFields = [
+    {
+      name: "name",
+      label: "Name",
+      type: "text",
+      required: true,
+      placeholder: "Enter Category Name",
+    },
+    {
+      name: "description",
+      label: "Description",
+      type: "text",
+      as: "textarea",
+      required: true,
+      placeholder: "write Information about the payment method",
+    },
+  ];
+  return (
+    <CustomModal>
+      <Form onSubmit={handleOnSubmit}>
+        <Form.Group>
+          <Form.Check
+            type="switch"
+            label="Active"
+            name="status"
+            onChange={handleOnChange}
+          />{" "}
+        </Form.Group>{" "}
+        {inputFields.map((item) => (
+          <CustomInoutFiedld
+            key={item.name}
+            {...item}
+            onChange={handleOnChange}
+          />
+        ))}
+        <Form.Group>
+          <Button variant="success" type="submit">
+            Update Payment Method{" "}
+          </Button>{" "}
+        </Form.Group>{" "}
+      </Form>{" "}
+    </CustomModal>
+  );
+};
