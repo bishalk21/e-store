abaafad65b222b36de7f1da85b8b8d8f618a572d
Your Name <karkibishal00@gmail.com>
2022-09-14 14:20:50 +1000

    products list to dashboard

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -1,25 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Col, Row } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 import { CustomCard } from "../../components/custom-card/CustomCard";
 import { CustomTable } from "../../components/custom-table/CustomTable";
 import { AdminLayout } from "../../components/layout/AdminLayout";
+import { getProductsAction } from "../products/productAction";
 
 const Dashboard = () => {
-  const clientTableHead = ["First Name", "Last Name", "joined date"];
+  const dispatch = useDispatch();
+  const { productList } = useSelector((state) => state.products);
 
+  useEffect(() => {
+    !productList.length && dispatch(getProductsAction());
+  }, [productList]);
+
+  const clientTableHead = ["First Name", "Last Name", "joined date"];
   const clientTableData = [
     {
       fName: "Bishal",
       lName: "Karki",
       joinedDate: "20-02-2022",
     },
-    {
-      fName: "Bishal",
-      lName: "Karki",
-      joinedDate: "20-02-2022",
-    },
   ];
-
   const orderTableHead = [
     "Status",
     "Product Name",
@@ -27,11 +30,11 @@ const Dashboard = () => {
     "Ordered Date",
     "Total",
   ];
-
   const orderTableData = [
     ["shipped", "Product Name", "Payment Status", "Ordered Date", "Total"],
   ];
 
+  const activeProduct = productList.filter((item) => item.status === "active");
   return (
     <AdminLayout>
       <h4>Dashboard</h4>
@@ -42,25 +45,31 @@ const Dashboard = () => {
         <hr />
         <Row className="g-4">
           <Col md={4}>
-            <CustomCard count="100" title=" Total Products" />
+            <CustomCard count={productList.length} title=" Total Products" />
           </Col>
           <Col md={4}>
-            <CustomCard count="48" title=" Total Products" />
+            <CustomCard count={activeProduct.length} title="Active" />
           </Col>
           <Col md={4}>
-            <CustomCard count="34" title=" Total Products" />
+            <CustomCard
+              count={productList.length - activeProduct.length}
+              title="Inactive"
+            />
           </Col>
         </Row>
       </div>
       {/* client summary */}
       <div className="clientSummary py-3">
-        <h4 className="fw-bold">Client Summary</h4>
+        <h4 className="fw-bold">Clients Details</h4>
         <hr />
         <CustomTable tableHead={clientTableHead} tableData={clientTableData} />
       </div>
       {/* order summary */}
       <div className="clientSummary py-3">
         <h4 className="fw-bold">Order Summary</h4>
+        <Link to="/orders" className="text-decoration-none">
+          <span className="text-primary">View All</span>
+        </Link>
         <hr />
         <CustomTable tableHead={orderTableHead} tableData={orderTableData} />
       </div>
